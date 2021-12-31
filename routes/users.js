57d1b203fc2684ba8457326228de96ee97facc48
Shanu Dey <sd4shanudey@gmail.com>
2021-12-31 07:35:34 +0000

    param in router
    Signed-off-by: Shanu Dey <sd4shanudey@gmail.com>

--- routes/users.js
+++ routes/users.js
@@ -9,8 +9,24 @@ router.get('/new', (req, res) => {
     res.send('New user form')
 })
 
-router.get('/:id', (req, res) => {
-    res.send(`Get the user with ID: ${req.params.id}`)
+router
+    .route('/:id')
+    .get((req, res) => {
+        res.send(`Get the user with ID: ${req.params.id}`)
+    })
+    .post((req, res) => {
+        res.send('Create user')
+    })
+    .put((req, res) => {
+        res.send('Update User')
+    })
+    .delete((req, res) => {
+        res.send('Delete user')
+    })
+
+router.param('id', (req, res, next, id) => {
+    console.log('ID=', id)
+    next()
 })
 
 module.exports = router
