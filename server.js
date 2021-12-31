c6cbc93f6c271ccdc1c323a4f8f717389357c613
Shanu Dey <sd4shanudey@gmail.com>
2021-12-31 05:41:39 +0000

    response to download a file from sever
    Signed-off-by: Shanu Dey <sd4shanudey@gmail.com>

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-    res.json({'hello':'world'})
+    res.download('server.js')
 })
 
 app.listen(process.env.PORT || 3000)
