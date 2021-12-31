55c279cb7678160e4ccdab353b619b8935048a93
Shanu Dey <sd4shanudey@gmail.com>
2021-12-31 07:44:30 +0000

    middleware
    Signed-off-by: Shanu Dey <sd4shanudey@gmail.com>

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 
 app.set('view engine', 'ejs')
+app.use(logger)
 
 app.get('/', (req, res) => {
     res.render('index', {text: 'World'})
@@ -9,5 +10,10 @@ app.get('/', (req, res) => {
 
 const userRouter = require('./routes/users')
 app.use('/users', userRouter)
+
+function logger(req, res, next){
+    console.log('Url = ', req.originalUrl)
+    next()
+}
 
 app.listen(process.env.PORT || 3000)
