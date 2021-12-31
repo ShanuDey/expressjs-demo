d0835716b891a12ef08fc768d362adb4a4cff44a
Shanu Dey <sd4shanudey@gmail.com>
2021-12-31 06:07:08 +0000

    added parameters in render views
    Signed-off-by: Shanu Dey <sd4shanudey@gmail.com>

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express()
 app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => {
-    res.render('index')
+    res.render('index', {text: 'World'})
 })
 
 app.listen(process.env.PORT || 3000)
